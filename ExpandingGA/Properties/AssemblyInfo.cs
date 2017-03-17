84a723eef247fbb59cf0cc8a957aa21c42fbf336
glupingane <torstein_alvern5557@hotmail.com>
2017-03-17 15:02:12 +0100

    structure changes

--- ExpandingGA/Properties/AssemblyInfo.cs
+++ ExpandingGA/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ExpandingGA")]
+[assembly: AssemblyTitle("GeneticAlgorithmForStrings")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ExpandingGA")]
+[assembly: AssemblyProduct("GeneticAlgorithmForStrings")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
